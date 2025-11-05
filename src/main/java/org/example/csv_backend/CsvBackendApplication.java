acefc0afc06b83b8e7ee9fa2623d62eb168be740
AO-5002 <ao2005ortiz@gmail.com>
2025-11-05 16:16:21 -0600

    git init

---
+++ src/main/java/org/example/csv_backend/CsvBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.csv_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsvBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CsvBackendApplication.class, args);
+    }
+
+}
